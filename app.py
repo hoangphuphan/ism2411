b9f567d6515fc35b6118a9be13b94a410c6857dd
hoangphuphan <phuphan@usf.edu>
2025-11-20 11:38:29 -0500

    Add formatted output displaying calculation results

--- app.py
+++ app.py
@@ -2,3 +2,4 @@ print("Welcome to my Python program!")
 minutes = input("How many minutes did you drive today?")
 minutes = float(minutes)
 weeklky_minutes = minutes * 7
+print(f"Your estimated total driving time this week is {weeklky_minutes}")
