54e954631838097d44b8f1828331e8cf685d47a2
hoangphuphan <phuphan@usf.edu>
2025-11-20 12:45:25 -0500

    Clean up code, add comments, finalize program

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 print("Welcome to my Python program!")
+#User's Input
 minutes = input("How many minutes did you drive today?")
-minutes = float(minutes)
+
+#Error Handling
+while True:
+    try:
+        minutes = float(minutes)
+        break
+    except ValueError:
+        print("Please enter a valid number.")
+        minutes = input()
+
+#Unit Conversion and Calculation
 weeklky_minutes = minutes * 7
 print(f"Your estimated total driving time this week is {weeklky_minutes}")
-
-try:
-    minutes = float(minutes)
-except ValueError:
-    print("Please enter a valid number.")
-    exit()
-
-
