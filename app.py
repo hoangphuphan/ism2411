1c2081fc752173413542cd8e6def62d20d488d47
hoangphuphan <phuphan@usf.edu>
2025-11-20 11:41:31 -0500

    Add basic error handling for user input

--- app.py
+++ app.py
@@ -3,3 +3,11 @@ minutes = input("How many minutes did you drive today?")
 minutes = float(minutes)
 weeklky_minutes = minutes * 7
 print(f"Your estimated total driving time this week is {weeklky_minutes}")
+
+try:
+    minutes = float(minutes)
+except ValueError:
+    print("Please enter a valid number.")
+    exit()
+
+
